e54399f93e4209663ca0c66448b7ac0de547d31d
Rohini Joshi <rohini.joshi@digitalcues.com>
2018-08-12 20:42:37 +0530

    First Commit

---
+++ TestGradleSpringBootExample/src/main/java/tester/Test.java
@@ -0,0 +1,11 @@
+package tester;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to gradle spring boot example");
+
+	}
+
+}
