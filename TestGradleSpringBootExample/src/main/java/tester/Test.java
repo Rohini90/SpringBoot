49f9d32d8d3fdf3f3da77d684a282b7502d794fe
palak sharma <spalak7@gmail.com>
2018-08-14 10:31:21 +0530

    i commit my changes in test.java

--- TestGradleSpringBootExample/src/main/java/tester/Test.java
+++ TestGradleSpringBootExample/src/main/java/tester/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to gradle spring boot example");
-
+        System.out.println("welcome in digital cues");
 	}
 
 }
